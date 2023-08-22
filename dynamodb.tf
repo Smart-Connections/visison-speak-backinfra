db163ce2b9271cf560869cba7aa49f8b94666bca
Sato Shingo <shinngoll@hotmail.co.jp>
2023-08-23 04:31:29 +0900

    チャット関連のテーブルを作成

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,33 +1,98 @@
-resource "aws_dynamodb_table" "users_table" {
-  name           = "Users-${var.environment}"
+resource "aws_dynamodb_table" "chat_threads" {
+  name           = "chat_threads-${var.environment}"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
-  range_key      = "Name"
+  read_capacity  = 5
+  write_capacity = 5
 
   attribute {
-    name = "UserId"
+    name = "chat_thread_id"
     type = "S"
   }
 
   attribute {
-    name = "Name"
+    name = "cognito_user_id"
     type = "S"
   }
 
   attribute {
-    name = "Timestamp"
+    name = "image_path"
+    type = "S"
+  }
+
+  attribute {
+    name = "topic"
+    type = "S"
+  }
+
+  attribute {
+    name = "created_timestamp"
     type = "N"
   }
 
+  attribute {
+    name = "updated_timestamp"
+    type = "N"
+  }
+
+  hash_key  = "cognito_user_id"
+  range_key = "updated_timestamp"
+}
+
+resource "aws_dynamodb_table" "chat_messages" {
+  name           = "chat_messages-${var.environment}"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "chat_message_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "chat_thread_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "cognito_user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "sender_type"
+    type = "S"
+  }
+
+  attribute {
+    name = "english_message"
+    type = "S"
+  }
+
+  attribute {
+    name = "japanese_message"
+    type = "S"
+  }
+
+  attribute {
+    name = "read"
+    type = "BOOL"
+  }
+
+  attribute {
+    name = "created_timestamp"
+    type = "N"
+  }
+
+  hash_key  = "chat_thread_id"
+  range_key = "created_timestamp"
+
   global_secondary_index {
-    name               = "NameIndex"
-    hash_key           = "Name"
-    range_key          = "Timestamp"
-    write_capacity     = 10
-    read_capacity      = 10
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["UserId"]
+    name            = "cognito_user_index"
+    hash_key        = "cognito_user_id"
+    range_key       = "created_timestamp"
+    write_capacity  = 5
+    read_capacity   = 5
+    projection_type = "ALL"
   }
 }
