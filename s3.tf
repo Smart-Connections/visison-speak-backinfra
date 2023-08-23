a8aa98714e2587101073ffd536e1d08e671a91fc
Sato Shingo <shinngoll@hotmail.co.jp>
2023-08-23 23:56:48 +0900

    チャットで使う画像用のバケットを作成

---
+++ s3.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "vision_speack" {
+  bucket = "vision-speack-${var.environment}"
+}
+
+resource "aws_s3_bucket_public_access_block" "vision_speak" {
+  bucket                  = aws_s3_bucket.vision_speak.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "vision_speak" {
+  bucket = aws_s3_bucket.vision_speak.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
